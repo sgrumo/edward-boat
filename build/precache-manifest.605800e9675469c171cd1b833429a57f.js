35f95682c6cd781662bce9c1a0dd76b27d83bcf1
Medici Samuele <samuele.medici@antreem.com>
2019-10-02 10:51:07 +0200

    first commit

---
+++ build/precache-manifest.605800e9675469c171cd1b833429a57f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afe726dbad0d89d7ecdc6afea65ccfb4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "78bf44eb43433d633541",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "3eeaa09117942e7ad4b6",
+    "url": "/static/js/2.23733a34.chunk.js"
+  },
+  {
+    "revision": "78bf44eb43433d633541",
+    "url": "/static/js/main.1aae1acb.chunk.js"
+  },
+  {
+    "revision": "f4fb5819703f4354ca0b",
+    "url": "/static/js/runtime-main.dc1120b5.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
